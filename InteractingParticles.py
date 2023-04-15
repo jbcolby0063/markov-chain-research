e3f044c835b0085f753cb2bf499074232f35a1fc
Colby <jbcolby0063@gmail.com>
2023-04-15 15:34:08 -0500

    fix left and right index error

--- InteractingParticles.py
+++ InteractingParticles.py
@@ -4,6 +4,9 @@ particle_nums = 10
 particle_top = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0] # 1 = occupied, 0 = empty
 particle_bottom = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
 
+def is_in_range(x): # check if index is in range
+    return 0 <= x < particle_nums
+
 def drift_prob(q, n): # drift probabilities 
     driftLeftRate = q * (q**(2*n - 1) + q**(-2*n + 1))
     driftRightRate = (1/q) * ((1/q)**(2*n - 1) + (1/q)**(-2*n + 1))
@@ -21,16 +24,16 @@ def drift(t, q, n): # time (seconds), speed of drift, n
     while time.time() < time_end:
         # top
         if particle_top[i] == 1: # if occupied
-            left_i = (i - 1) % particle_nums # left particle
-            right_i = (i + 1) % particle_nums # right particle
+            left_i = i - 1 # left particle
+            right_i = i + 1 # right particle
             moved = False 
             # visit left empty particle first
-            if particle_top[left_i] == 0 and drift_choice(driftLeftProb):
+            if is_in_range(left_i) and particle_top[left_i] == 0 and drift_choice(driftLeftProb):
                 particle_top[left_i] = 1
                 particle_top[i] = 0
                 moved = True
             # if the particle didn't move to left, visit right empty particle
-            if not moved and particle_top[right_i] == 0 and drift_choice(driftRightProb):
+            if is_in_range(right_i) and not moved and particle_top[right_i] == 0 and drift_choice(driftRightProb):
                 particle_top[right_i] = 1
                 particle_top[i] = 0
                 moved = True
@@ -38,25 +41,24 @@ def drift(t, q, n): # time (seconds), speed of drift, n
         
         # bottom
         if particle_bottom[j] == 1: # if occupied
-            left_j = (j - 1) % particle_nums # left particle
-            right_j = (j + 1) % particle_nums # right particle
+            left_j = j - 1 # left particle
+            right_j = j + 1 # right particle
             moved = False 
             # visit left empty particle first
-            if particle_bottom[left_j] == 0 and drift_choice(driftLeftProb):
+            if is_in_range(left_j) and particle_bottom[left_j] == 0 and drift_choice(driftLeftProb):
                 particle_bottom[left_j] = 1
                 particle_bottom[j] = 0
                 moved = True
             # if the particle didn't move to left, visit right empty particle
-            if not moved and particle_bottom[right_j] == 0 and drift_choice(driftRightProb):
+            if is_in_range(right_j) and not moved and particle_bottom[right_j] == 0 and drift_choice(driftRightProb):
                 particle_bottom[right_j] = 1
                 particle_bottom[j] = 0
                 moved = True
         j = (j + 1) % particle_nums # keep moving index to right
-    # print result
-    for particle in particle_top:
-        print(particle, end=" ")
-    print("\n")
-    for particle in particle_bottom:
-        print(particle, end=" ")
 
-drift(25, 0.5, 3)
+    # print result
+    print(f"After drifting particles for {t} seconds with q={q} and n={n}: ")
+    print(*particle_top, sep=" ")
+    print(*particle_bottom, sep=" ")
+
+drift(5, 0.5, 3)
